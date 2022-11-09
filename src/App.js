44579efb836c16f781b3f348e3a35620e3e54a85
LeeviIlli <99171073+LeeviIlli@users.noreply.github.com>
2022-11-09 11:30:46 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div class="header">
+        <h1>Header here</h1>
+      </div>
+    
+      <div class="row">
+        <div class="col-3 navbar">
+          <button class="btn-1">Home</button>
+          <button class="btn-1">login</button> 
+          <button class="btn-2">sign in</button>
+          
+        </div>
+      </div>
+
+      <div class="col-6">
+        <h1>Blaa blaa</h1>
+        <p>Blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa 
+        blaa blaa blaa blaa blaa blaa blaa blaa blaa.
+        </p>
+      </div>
+      
+      <div class="col-3">
+        <div class="aside">
+          <h2>Jotaki</h2>
+          <p>Jotaki</p>
+          <h2>Jotaki</h2>
+          <p>Jotaki</p>
+          <h2>Jotaki</h2>
+          <p>Jotaki</p>
+          <h2>Jotaki</h2>
+          <p>Jotaki</p>
+         
+        </div>
+      </div>  
+        <div class="footer">
+      <p>Blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa.</p>
+        </div>
+    </div>
+    
+     
+  );
+}
+
+export default App;
